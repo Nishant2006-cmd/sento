424232a4003497c0e3dee44558203e6e56e45927
Nishant Sharma <nishant.1511977@gmail.com>
2026-07-31 12:42:57 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -99,6 +99,14 @@ def send_message(data):
         room=data["room"]
     )
 
+import os
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True,port=5001)
+    port = int(os.environ.get("PORT", 5001))
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
